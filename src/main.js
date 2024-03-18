d3996af0e6bd3eb36554e754e9c05829a2f69029
shawn-sw <146413305+shawn-sw@users.noreply.github.com>
2024-03-18 16:55:20 -0700

    Update main.js
    -Turn off debug
    -Redefine game scenes
    -Set initial position of the camera
    -Set initial variables for each enemy

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-// Code Practice: Slime World
+// Code Practice: 
 // Name: 
-// Date: 
+// Game include: physics systems, cameras, text objects, the animation manager,  and tilemaps
 'use strict'
 
 let config = {
@@ -18,13 +18,25 @@ let config = {
         }
     },
     zoom: 1.5,
-    scene: [ Menu,Demo,Endscreen ]
+    scene: [ Menu,Level1,Level2,Endscreen,Deadscreen,Credits ]
 }
 
 const game = new Phaser.Game(config)
 
 let keyRESET
+let keyC
 
-let xa = 130
-let xb = 130
-let xc = 130
+let camPos = 640
+
+let l1a = 130
+let l1b = 130
+let l1c = 130
+
+let l2c = 110
+let l1d = 110
+let l1e = 110
+let l1f = 110
+let l1g = 110
+let l1h = 110
+let l1i = 110
+let l1j = 110
